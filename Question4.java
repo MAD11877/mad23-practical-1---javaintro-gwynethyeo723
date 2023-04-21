d4315cc12f4d3e5b96c5b9b940b585766f766c5c
gwynethyeo723 <s10222843@connect.np.edu.sg>
2023-04-22 01:52:23 +0800

    finished!!

--- Question4.java
+++ Question4.java
@@ -4,35 +4,21 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
+    
     Scanner in = new Scanner(System.in);
     System.out.print("Enter an integer for the base of the triangle: ");
     int number = in.nextInt();
 
-    for (int j = 0; j<=number; j = j + 1)
+    System.out.println(">"+number);
+
+    for (int j = number; j > 0 ; j = j - 1)
     {
-      System.out.print("");
-      for (int i = 0; i <= number; i = i + 1)
+      for (int i = 0; i < j; i = i + 1)
     {
       System.out.print("*");
     }
+    System.out.println();
     }
     
-
-
-
-
   }
 }
